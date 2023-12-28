34224eec70588e01438df0f07fc6410c1148afd5
gokubha <154727394+gokubha@users.noreply.github.com>
2023-12-28 19:56:49 +0530

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+function db() {
+    mongoose.connect('mongodb:127.0.0.1:27017/mydata').then(() => {
+        console.log("database connected");
+    }).catch((err) => {
+        console.log("Eror in Dattaase", err);
+    })
+}
+

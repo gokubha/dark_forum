45f24b1fe0f9761e156bb8c8445b205ff3c3f47a
gokubha <154727394+gokubha@users.noreply.github.com>
2023-12-28 20:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 const db = require('./server/config/db')
+db()
 const seed = require('./server/config/seed')
 const adminRoutes = require("./server/Routes/adminRoutes")
 const userRoutes = require("./server/Routes/userRoutes")
 
-app.use(express.urlencoded({extended:false, limit:'50mb', parameterLimit:3000}))
+app.use(express.urlencoded({ extended: false, limit: '50mb', parameterLimit: 3000 }))
 
 app.use("/user", userRoutes)
 app.use("/admin", adminRoutes)
 
 
-app.listen(4000,(err)=>{
-  if(err){
+app.listen(4000, (err) => {
+  if (err) {
     console.log(err);
-  }else{
+  } else {
     console.log("server is running");
   }
 })

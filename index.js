46a5e925827c8d292cdfb6290bfca4c564c8e733
gokubha <154727394+gokubha@users.noreply.github.com>
2024-01-05 14:43:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const app = express()
 const db = require('./server/config/db')
-var cors = require('cors')
-db()
 const seed = require('./server/config/seed')
-const adminRoutes = require("./server/Routes/adminRoutes")
-const userRoutes = require("./server/Routes/userRoutes")
+const mongoose = require('mongoose')
+var cors = require('cors')
+var path = require('path')
 app.use(cors())
 
-app.use(express.urlencoded({ extended: false, limit: '50mb', parameterLimit: 3000 }))
+db()
 
-app.get("/",(req,res)=>{
-  res.send("💀💀💀FUCK YOU BRO💀💀💀")
-})
+app.use(express.urlencoded({ extended: false, limit: '50mb', parameterLimit: 30000000 }))
+app.use(express.static(path.join(__dirname,'/server/public')));
+
+
+const adminRoutes = require("./server/Routes/adminRoutes")
+const userRoutes = require("./server/Routes/userRoutes")
 
 app.use("/user", userRoutes)
 app.use("/admin", adminRoutes)
 
+app.get("/",(req,res)=>{
+  res.send("💀💀💀YO00 BRO💀💀💀")
+})
 
-app.listen(4000, (err) => {
+app.listen(5000, (err) => {
   if (err) {
     console.log(err);
   } else {
